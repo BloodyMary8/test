edc1a28d7d2370d33f4041eb639c0786eb3b4d0d
BloodyMary8 <masharabota1996@gmail.com>
2021-12-20 21:47:18 +0700

    bla blsa

---
+++ src/Matcher.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+    public class Matcher {
+        public static void main(String[] args) {
+            String text = "name@gmail.com @gmail.com";
+            Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+            Matcher matcher = pattern.matcher(text);
+
+            while (matcher.find()) {
+                int start = matcher.start();
+                int end = matcher.end();
+                System.out.println(text.substring(matcher.start(), matcher.end()));
+            }
+        }
+    }
+
